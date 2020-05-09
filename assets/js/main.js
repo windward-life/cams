384fde61989aa549acd79cb8a4e5a64a2d044a5b
Daniel Aakjær Sørensen <daniel.aakjaer@gmail.com>
2020-05-08 20:25:00 -0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  // Page preloading animation
+  $('.animsition').animsition({
+      inDuration: 800,
+      outDuration: 400,
+      linkElement: '.switch-page',
+      timeout: false,
+      timeoutCountdown: 5000,
+      onLoadEvent: true,
+      transition: function(url){ window.location.href = url; }
+  });
+});
